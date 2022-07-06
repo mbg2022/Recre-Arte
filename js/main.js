e7ad147f55dd400a45c4234ff4e932479c879427
mbg <mbgagliardi@gmail.com>
2022-07-06 10:03:43 -0300

    SegundaEntrega

--- js/main.js
+++ js/main.js
@@ -1,34 +1,16 @@
-//guardarLocal();
-
-//const { each } = require("lodash");
-
-//guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
-// si quiero  almacenar el array completo (solo prueba)
-//guardarLocal("StorageActividades", JSON.stringify(actividad));
-/*class Actividad {
-    constructor(obj) {
-        this.activity_name = obj.activity_name;
-        this.activity_date = obj.activity_date
-        this.activity_value = parseFloat(obj.activity_value);
-        this.location = obj.location;
-        this.country = obj.country;
-
-    }
-}*/
-
 let containerActividades = document.getElementById("containerActividades");
 let alertProximasActividades = document.getElementById("alertProximasActividades");
 let contenedor_fav = document.getElementById("contenidoFavoritos");
 
+
 let actividades = [];
-let actividades1 = [];
-let favoritos = [];
+let actividadesGuardadas = [];
+//let actividadesCreadasPorMi = []; Queda para seguir con la funcionalidad pensada
+let actividadesFavoritas = [];
 
-//localStorage.setItem("StorageActividades", JSON.stringify(actividad));
 
-localStorage.removeItem("StorageFavoritos");
 cargarActividades();
-cargarFavoritos();
+cargarActividadesFavoritas();
 
 mostrarActividades();
 mostrarActividadesFavoritas();
@@ -36,89 +18,87 @@ mostrarActividadesFavoritas();
 
 /*Inicio Actividades*/
 class Actividades {
-    constructor(nombreActividad, valorActividad, fechaActividad) {
-        this.nombreActividad = nombreActividad
-        this.valorActividad = valorActividad
-        this.fechaActividad = fechaActividad
+    constructor(id, activity_name, activity_date, activity_value, locat, country) {
+        this.id = id;
+        this.activity_name = activity_name;
+        this.activity_date = activity_date;
+        this.activity_value = activity_value;
+        this.locat = locat;
+        this.country = country;
     }
-    calcularCuantosDiasFaltan(nombreActividad, fechaActividad, fechaHoy) {
-        let difMiliseg = Math.abs(fechaActividad - Date.parse(fechaHoy))
+    calcularCuantosDiasFaltan(activity_name, activity_date, fechaHoy) {
+        let difMiliseg = Math.abs(activity_date - Date.parse(fechaHoy))
         let resta = difMiliseg / (1000 * 60 * 60 * 24)
     }
 }
 
 
-
-
-
 function cargarActividades() {
-    if (localStorage.getItem("StorageActividades") !== null) { //si existe
-        console.log("pasa por actividades");
-        actividades = JSON.parse(localStorage.getItem("StorageActividades"));
-        return;
-    } else { //Si no existe lo creo porque es la primera vez que abro la pagina
-        localStorage.setItem("StorageActividades" /*Clave*/ , JSON.stringify(actividad)); //Acá estoy convirtiendo actividad en un string para almacenarloen el LocalStorage
-        return;
-    }
-}
-
-//Carga actividades favoriteadas en el LocalStorage
-function cargarFavoritos() {
-    if (localStorage.getItem("StorageFavoritos") !== null) { //Si existe
-        console.log("pasa por las actividades Favoritas cargadas");
-        favoritos = JSON.parse(localStorage.getItem("StorageFavoritos"));
-        favoritos.forEach(element => {
-            console.log("nombre de (actividad)elemento: " + element.activity_name);
-        })
+    if (localStorage.getItem("StorageActividades") !== null) { //si existe    
+        actividadesGuardadas = JSON.parse(localStorage.getItem("StorageActividades"));
         return;
     } else {
-        localStorage.setItem("StorageFavoritos", JSON.stringify(favoritos));
-        console.log("No tiene actividades Favoritas cargadas");
         return;
     }
 }
 
+
+function cargarActividadesFavoritas() {
+    if (localStorage.getItem("StorageActividadesFavoritas") !== null) { //si existe    
+        actividadesFavoritas = JSON.parse(localStorage.getItem("StorageActividadesFavoritas"));
+        return;
+    } else {
+        console.log("no hay actividades favoritas")
+        return;
+    }
+}
+
+
+
 function mostrarActividades() {
-    actividad.forEach((actividad) => {
+    actividadesGuardadas.forEach((actividadesGuardadas) => {
         containerActividades.innerHTML += `
         <div class="card border-primary mb-3" style="max-width: 20rem;">
-        <div class="card-header">${actividad.activity_name}</div>
+        <div class="card-header">${actividadesGuardadas.activity_name}</div>
         <div class="card-body">
-            <h4 class="card-title">${(actividad.activity_date)}</h4>
-            <h5>${actividad.location}</h5>
-            <h5>Faltan ${calcularCuantosDiasFaltan(actividad.activity_date)} días</h5>
-            <p class="card-text">$ ${actividad.activity_value}</p>
-            <button onClick="actividadFavorita(${actividad})" class="btn btn-outline-success">Favoritear</button>
+            <h4 class="card-title">${(actividadesGuardadas.activity_date)}</h4>
+            <h5>${actividadesGuardadas.locat}</h5>
+            <h5>Faltan ${calcularCuantosDiasFaltan(actividadesGuardadas.activity_date)} días</h5>
+            <p class="card-text">$ ${actividadesGuardadas.activity_value}</p>
+            <button onClick="actividadFavorita(${actividadesGuardadas.id})" class="btn btn-outline-success">Favoritear</button>
+            <button onClick="eliminarActividad(${actividadesGuardadas.id})" class="btn btn-outline-success">Eliminar</button>
         </div>
         </div>
         `;
-
     });
 }
 
 
 function mostrarActividadesFavoritas() {
-    favoritos.forEach((favoritos) => {
-        console.log("pOR MOSTRAR ACTIVIDADES fAVORITAS: " + favoritos);
-        contenidoFavoritos.innerHTML += `
+    actividadesFavoritas.forEach((actividadesFavoritas) => {
+        containerActividadesFavoritos.innerHTML += `
         <div class="card border-primary mb-3" style="max-width: 20rem;">
-        <div class="card-header">${favoritos.activity_name}</div>
+        <div class="card-header">${actividadesFavoritas.activity_name}</div>
         <div class="card-body">
-            <h4 class="card-title">${(favoritos.activity_date)}</h4>
-            <h5>${favoritos.location}</h5>
-            <h5>Faltan ${calcularCuantosDiasFaltan(favoritos.activity_date)} días</h5>
-            <p class="card-text">$ ${favoritos.activity_value}</p>
-            <button onClick="eliminarFavorita(${favoritos.id})" class="btn btn-outline-success">Des Favoritear</button>
+            <h4 class="card-title">${actividadesFavoritas.activity_date}</h4>
+            <h5>${actividadesFavoritas.locat}</h5>
+            <h5>Faltan ${calcularCuantosDiasFaltan(actividadesFavoritas.activity_date)} días</h5>
+            <p class="card-text">$ ${actividadesFavoritas.activity_value}</p>
+            <button onClick="eliminarActividadFavorita(${actividadesFavoritas.id})" class="btn btn-outline-success">Eliminar Fav</button>
         </div>
         </div>
         `;
-
     });
 }
 
 
+function resetearLocalStore() {
+    localStorage.removeItem("StorageActividadesFavoritas");
+    localStorage.removeItem("StorageActividades");
+    //localStorage.removeItem("StorageActividadesCreadasPorMi");
+    location.reload();
 
-
+}
 
 function formatearFechaDDMMAA(fecha) {
     let date = new Date(fecha);
@@ -132,8 +112,6 @@ function calcularCuantosDiasFaltan(fechaActividad) {
     let difMiliseg = Math.abs(Date.parse(fechaActividad) - Date.parse(fechaHoy))
     let resta = Math.round(difMiliseg / (1000 * 60 * 60 * 24))
     return resta;
-
-
 }
 
 function calcularDistancia(fechaActividad) {
@@ -141,91 +119,72 @@ function calcularDistancia(fechaActividad) {
     let difMiliseg = Math.abs(Date.parse(fechaActividad) - Date.parse(fechaHoy))
     let resta = Math.round(difMiliseg / (1000 * 60 * 60 * 24))
     return resta;
-
 }
 
-/*Falta seguir desarrollando, la idea es  que me calcule la distancia desde la ubicación donde estoy hasta la actividad
-getKilometros = function(lat1, lon1, lat2, lon2) {
-    rad = function(x) { return x * Math.PI / 180; }
-    var R = 6378.137; //Radio de la tierra en km
-    var dLat = rad(lat2 - lat1);
-    var dLong = rad(lon2 - lon1);
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(rad(lat1)) * Math.cos(rad(lat2)) * Math.sin(dLong / 2) * Math.sin(dLong / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c;
-    return d.toFixed(3); //Retorna tres decimales
-}*/
 
 /*Levanta las Actividades del form*/
+function crearActividad() {
+    activity_name = document.getElementById("activity_name").value;
+    activity_date = document.getElementById("activity_date").value;
+    activity_value = document.getElementById("activity_value").value;
+    country = document.getElementById("country").value;
+    locat = document.getElementById("locat").value;
 
-function obtenerDatos() {
-    nombreActividad = document.getElementById("nombreActividad").value;
-    valorActividad = document.getElementById("valorActividad").value;
-    fechaActividad = document.getElementById("fechaActividad").value;
-    let actividad = [nombreActividad, valorActividad, fechaActividad];
+    //Guarda al Array en el que están TODAS las actividades existentes
+    let indice = actividadesGuardadas.length;
+    actividadesGuardadas.push(new Actividades(indice + 1, activity_name, activity_date, activity_value, locat, country));
 
-    actividades.push(new Actividades(nombreActividad, valorActividad, fechaActividad));
+    /*
+    let indice2 = actividadesCreadasPorMi.length;
+    //Guarda al Array para distinguir que fueron las actividades que cree yo(esto para más addelante)
+    actividadesCreadasPorMi.push(new Actividades(indice + 1, activity_name, activity_date, activity_value, locat, country));*/
 
-    let diasRestantes = calcularCuantosDiasFaltan(fechaActividad);
-
-    let contenedor = document.querySelector('#contenido');
-    let p = document.createElement('p');
-    p.innerText = `Se creó la actividad : ${ nombreActividad},  faltan : ${diasRestantes} días, el valor de la actividad es de: ${valorActividad}   Pesos`;
-    // contenedor.appendChild(p);
-
-    p.innerText = `Se creó la actividad : ${ nombreActividad},  faltan : ${diasRestantes} días, el valor de la actividad es de: ${valorActividad}   Pesos`;
-
-    contenedor.appendChild(p);
-
-    contenedor.innerHTML += `
-        <div class="card border-primary mb-3" style="max-width: 20rem;">
-        <div class="card-header">${nombreActividad}</div>
-        <div class="card-body">
-            <h4 class="card-title"></h4>
-            <h5></h5>
-            <h5>Faltan ${diasRestantes}  días</h5>
-            <p class="card-text">$ ${valorActividad}</p>
-            <div class="col-12 align">
-            <button type="submit" class="btn btn-primary" id="botonEliminar" onclick="EliminarDatos()" value="Eliminar actividad">Eliminar</button>
-        </div>
-        </div>
-        </div>
-        `;
-
-
-
-
-};
-
-function EliminarDatos() {
-
+    location.reload()
+    localStorage.setItem("StorageActividades", JSON.stringify(actividadesGuardadas));
+    // localStorage.setItem("StorageActividadesCreadasPorMi", JSON.stringify(actividadesCreadasPorMi));
+    location.reload()
 };
 
 
-
-//FUNCION AGREGAR USUARIO FAVORITO AL ARRAY
-function actividadFavorita(actividad) {
-    // let indice = identificador - 1;
-    //alert("Identificador" + identificador);
+//Agrega la actividad Favorita al array de Favoritos
+function actividadFavorita(identificador) {
+    let indice = identificador - 1;
     let objeto_seleccionado = {};
-    // objeto_seleccionado = favoritos[indice];
-    objeto_seleccionado = actividad;
-    alert("objeto Seleccinado¨actividad " + objeto_seleccionado);
-    alert("objeto Seleccinado " + favoritos.length + 1);
-    if (!favoritos.some(e => e.id === identificador)) {
-        favoritos.push(objeto_seleccionado);
-        localStorage.setItem("StorageFavoritos", JSON.stringify(favoritos));
-        // location.reload()
+    objeto_seleccionado = actividadesGuardadas[indice];
+    if (!actividadesFavoritas.some(e => e.id === identificador)) {
+        actividadesFavoritas.push(objeto_seleccionado);
+        location.reload()
+        localStorage.setItem("StorageActividadesFavoritas", JSON.stringify(actividadesFavoritas));
+        location.reload()
     } else {
-        alert("La actividad ya se encuentra cargada en favoritos")
+        alert("La actividad ya se encuentra en favoritos =)")
     }
 }
 
+
 //Elimina la actividad del array de favoritos
-function eliminarFavorita(id) {
-    let favoritos_filtrado = favoritos.filter((elemento) => elemento.id != id);
-    favoritos = favoritos_filtrado;
-    localStorage.setItem("StorageFavoritos", JSON.stringify(favoritos));
-    console.log(favoritos_filtrado);
+function eliminarActividadFavorita(id) {
+    let favoritos_filtrado = actividadesFavoritas.filter((elemento) => elemento.id != id);
+    actividadesFavoritas = favoritos_filtrado;
+    if (actividadesFavoritas.length > 0) { //para que no guarde el array vacio
+        localStorage.setItem("StorageActividadesFavoritas", JSON.stringify(actividadesFavoritas));
+        location.reload();
+    } else {
+        localStorage.removeItem("StorageActividadesFavoritas");
+        location.reload();
+    }
+}
+
+
+//Elimina la actividad del array actividadesGuardadas
+function eliminarActividad(id) {
+    let actividadesGuardadas_filtrado = actividadesGuardadas.filter((elemento) => elemento.id != id);
+    actividadesGuardadas = actividadesGuardadas_filtrado;
+    localStorage.setItem("StorageActividades", JSON.stringify(actividadesGuardadas));
+    eliminarActividadFavorita(id) //Elimina tambien la actividad de favoritas
     location.reload();
+}
+
+function cargarArrayActividades(actividad) {
+    actividades.push(actividad);
 }
